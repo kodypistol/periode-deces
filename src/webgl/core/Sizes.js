67650e3a1e178d5480f7abccc4c65ef9cf0aab40
flanb <clement.renou.47@gmail.com>
2024-10-18 04:10:33 +0200

    🐛 (Sizes): debug if debug active

--- src/webgl/core/Sizes.js
+++ src/webgl/core/Sizes.js
@@ -23,18 +23,19 @@ export default class Sizes extends EventEmitter {
 		})
 
 		this.experience = new Experience()
-		this.experience.debug.ui
-			.addBinding(this, 'downScale', {
-				label: 'Down scale',
-				min: 1,
-				max: 5,
-				step: 1,
-			})
-			.on('change', () => {
-				this.width = window.innerWidth / this.downScale
-				this.height = window.innerHeight / this.downScale
-				this.trigger('resize')
-			})
+		if (this.experience.debug.active)
+			this.experience.debug.ui
+				.addBinding(this, 'downScale', {
+					label: 'Down scale',
+					min: 1,
+					max: 5,
+					step: 1,
+				})
+				.on('change', () => {
+					this.width = window.innerWidth / this.downScale
+					this.height = window.innerHeight / this.downScale
+					this.trigger('resize')
+				})
 	}
 	#checkIfMobile() {
 		const isMobileUa = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
