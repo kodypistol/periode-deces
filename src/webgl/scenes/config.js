6491ed0cd8423facd6ef9aeabbce839106f0b838
Lucien <lucien.boisseau-sable@smice.com>
2024-10-20 22:22:11 +0200

    🔥 Removing Phone Scene

--- src/webgl/scenes/config.js
+++ src/webgl/scenes/config.js
@@ -1,11 +1,9 @@
 import Main from './Main'
 import Computer from './Computer'
-import PhoneScene from './PhoneScene'
 import SingleCube from './SingleCube.js'
 
 export default {
 	main: Main,
 	computer: Computer,
-	phoneScene: PhoneScene,
 	'single-cube': SingleCube,
 }
